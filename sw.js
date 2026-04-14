38b501703031f93abdd920de3c28bd3556f19a7b
一ツ山翔平 <hitotsuyamashohei@MacBook-Air-3.local>
2026-04-15 05:43:45 +0900

    配車管理アプリ初回アップロード

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'haisha-v1';
+const urlsToCache = ['index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache)));
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(names =>
+      Promise.all(names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n)))
+    )
+  );
+});
